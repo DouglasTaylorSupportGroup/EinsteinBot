801cf6b327fd0c7820bc873f78ad74d97dc198ce
lerodr <luzzipoo@gmail.com>
2021-12-28 02:12:25 -0700

    add help command

--- cogs/info.py
+++ cogs/info.py
@@ -13,5 +13,14 @@ class Information(commands.Cog):
     async def ping(self, ctx):
         await ctx.respond(f"🏓 Pong! My ping is {round(self.bot.latency * 1000)}ms")
 
+    @commands.command(name="help")
+    async def help(self, ctx):
+        embed = discord.Embed(title="Help", color=0x3083e3)
+        embed.set_author(name="EinsteinBot", icon_url=self.bot.user.avatar.url)
+        embed.add_field(name="help", value="Display this message.", inline=False)
+        embed.add_field(name="ping", value="Displays the ping.", inline=False)
+        embed.add_field(name="search `url`", value="Searches for the answer within a Chegg link.", inline=False)
+        await ctx.respond(embed=embed)
+
 def setup(bot):
     bot.add_cog(Information(bot))
