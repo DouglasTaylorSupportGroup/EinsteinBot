fcf25e9157842d59ba3cc812dd1ec120ff7acb8d
Jack Pee <mcinvasion2016@gmail.com>
2022-01-15 19:58:35 -0700

    refactor: change commands to use util functions

--- cogs/info.py
+++ cogs/info.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+from core.utils import send
+
 class Information(commands.Cog):
     
     # Constructor
@@ -11,10 +13,7 @@ class Information(commands.Cog):
     @commands.command(name="ping")
     async def ping(self, ctx):
         ping = f"🏓 Pong! My ping is {round(self.bot.latency * 1000)}ms"
-        if isinstance(ctx, discord.ApplicationContext):
-            await ctx.respond(ping)
-        else:
-            await ctx.send(ping)
+        await send(ctx, ping, False)
 
     @commands.command(name="help")
     async def help(self, ctx):
@@ -23,10 +22,7 @@ class Information(commands.Cog):
         embed.add_field(name="=help", value="Display this message.", inline=False)
         embed.add_field(name="=ping", value="Displays the ping.", inline=False)
         embed.add_field(name="=search `url`", value="Searches for the answer within a Chegg link.", inline=False)
-        if isinstance(ctx, discord.ApplicationContext):
-            await ctx.respond(embed=embed)
-        else:
-            await ctx.send(embed=embed)
+        await send(ctx, embed, True)
 
 def setup(bot):
     bot.add_cog(Information(bot))
--- cogs/slashCommands.py
+++ cogs/slashCommands.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
-from cogs import info
+from cogs import info, main
+from core.utils import send
+from core.ratelimit import ratelimit
 
 class SlashCommands(commands.Cog):
     def __init__(self, bot):
@@ -13,6 +15,25 @@ class SlashCommands(commands.Cog):
     @commands.slash_command(name="help", guild_ids=[642556556680101903])
     async def help_slash(self, ctx):
         await info.Information.help(ctx)
+
+    @commands.slash_command(name="search", guild_ids=[642556556680101903])
+    @ratelimit.searchCooldown
+    async def search_slash(self, ctx, **url):
+        await ctx.defer()
+        url = url["url"]
+        await main.Commands.search(ctx, url)
+
+    @search_slash.error
+    async def search_slash_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            embed = discord.Embed(title="Global Rate-Limit", description=f"Please wait {round(error.retry_after)} seconds and try again.", color=0xeb7100)
+            embed.set_footer(text="Avoids bot detection on Chegg.")
+            await send(ctx, embed, True)
+        else:
+            completeError = discord.Embed(title="Error", color=0xff4f4f, description="Something went wrong. Create an issue here for support: https://github.com/DouglasTaylorSupportGroup/EinsteinBot")
+            completeError.set_footer(text="ERROR002")
+            await send(ctx, completeError, True)
+            raise error
 
 def setup(bot):
     bot.add_cog(SlashCommands(bot))
