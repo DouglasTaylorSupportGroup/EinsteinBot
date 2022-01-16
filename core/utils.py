e8be016e2da6a4b262fda7443dbe00cf34b35855
Jack Pee <mcinvasion2016@gmail.com>
2022-01-15 19:57:42 -0700

    refactor: send & senddefer functions

--- core/utils.py
+++ core/utils.py
@@ -11,3 +11,15 @@ async def send(ctx, message, isEmbed):
             await ctx.respond(message)
         else:
             await ctx.send(message)
+
+async def sendDefer(ctx, message, isEmbed):
+    if isEmbed:
+        if isinstance(ctx, discord.ApplicationContext):
+            await ctx.send_followup(embed=message)
+        else:
+            await ctx.send(embed=message)
+    else:
+        if isinstance(ctx, discord.ApplicationContext):
+            await ctx.send_followup(message)
+        else:
+            await ctx.send(message)
